76f40f66c92350a886b5b9469d56bde930d6ee3f
Ali <naghinjadali@gmail.com>
2025-08-10 16:50:07 +0330

    add Auth to project

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,11 +150,13 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+   "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
 }
 
 DJOSER = {
     'SERIALIZERS':{
             'user_create': 'core.serializers.UserCreateSerializer',
+            'current_user': 'core.serializers.UserSerializer',
     }
 }
 
